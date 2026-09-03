2df2c6a308475a3f5d7dc2443f0e5962e951b33e
mohammedfasin0-ai <mohammedfasin0@gmail.com>
2026-09-03 11:06:08 +0530

    minor change

--- collectors/reddit/comment_crawler.py
+++ collectors/reddit/comment_crawler.py
@@ -32,7 +32,7 @@ def crawl_comments():
 
             print(f"Marked {post['post_id']}) as comments scraped!")
 
-            time.sleep(3)
+            time.sleep(5)
 
         except Exception as e:
             print(f"Failed to process post {post['post_id']}")
